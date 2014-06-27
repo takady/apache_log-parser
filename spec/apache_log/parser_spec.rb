d2c10dc08db9ec75bee1008a7985410264d88117
Yuichi Takada <takadyy@gmail.com>
2014-06-28 00:29:15 +0900

    add rspec testcase

--- spec/apache_log/parser_spec.rb
+++ spec/apache_log/parser_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
+require 'date'
 
 describe ApacheLog::Parser do
   it 'has a version number' do
     expect(ApacheLog::Parser::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can parse combined format log' do
+    parser = ApacheLog::Parser.getParser('combined')
+    line = '104.24.160.39 - - [07/Jun/2014:14:58:55 +0900] "GET /category/electronics HTTP/1.1" 200 128 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0.1) Gecko/20100101 Firefox/9.0.1"'
+    entity = parser.parse(line.chomp)
+
+    expect = {
+      remote_host:    '104.24.160.39',
+      identity_check: '-',
+      user:           '-',
+      datetime:       DateTime.new(2014, 6, 7, 14, 58, 55, 0.375),
+      request:        {
+                        method:   'GET',
+                        path:     '/category/electronics',
+                        protocol: 'HTTP/1.1',
+                      },
+      status:         '200',
+      size:           '128',
+      referer:        '-',
+      user_agent:     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0.1) Gecko/20100101 Firefox/9.0.1',
+    }
+
+    expect(entity).to eq(expect)
   end
 end
